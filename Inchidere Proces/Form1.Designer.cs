b51c32ccaf301b0c509104b57e593075a0ff96a3
alina <alina@DESKTOP-C5BO00I>
2022-11-15 15:24:59 +0200

    Add project files.

---
+++ Inchidere Proces/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Inchidere_Proces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_schimbul1 = new System.Windows.Forms.TextBox();
+            this.txt_schimbul2 = new System.Windows.Forms.TextBox();
+            this.txt_schimbul3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.OldLace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "                  ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Orele la care se restarteaza calculatorul";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Schimbul 1";
+            // 
+            // txt_schimbul1
+            // 
+            this.txt_schimbul1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txt_schimbul1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_schimbul1.Location = new System.Drawing.Point(49, 64);
+            this.txt_schimbul1.Name = "txt_schimbul1";
+            this.txt_schimbul1.ReadOnly = true;
+            this.txt_schimbul1.Size = new System.Drawing.Size(100, 27);
+            this.txt_schimbul1.TabIndex = 4;
+            this.txt_schimbul1.TabStop = false;
+            this.txt_schimbul1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_schimbul2
+            // 
+            this.txt_schimbul2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txt_schimbul2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_schimbul2.Location = new System.Drawing.Point(228, 64);
+            this.txt_schimbul2.Name = "txt_schimbul2";
+            this.txt_schimbul2.ReadOnly = true;
+            this.txt_schimbul2.Size = new System.Drawing.Size(100, 27);
+            this.txt_schimbul2.TabIndex = 4;
+            this.txt_schimbul2.TabStop = false;
+            this.txt_schimbul2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_schimbul3
+            // 
+            this.txt_schimbul3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txt_schimbul3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_schimbul3.Location = new System.Drawing.Point(407, 64);
+            this.txt_schimbul3.Name = "txt_schimbul3";
+            this.txt_schimbul3.ReadOnly = true;
+            this.txt_schimbul3.Size = new System.Drawing.Size(100, 27);
+            this.txt_schimbul3.TabIndex = 4;
+            this.txt_schimbul3.TabStop = false;
+            this.txt_schimbul3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(230, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Schimbul 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(411, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Schimbul 3";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(45, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Urmatoarea restartare:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.PeachPuff;
+            this.label7.Location = new System.Drawing.Point(49, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(339, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Pentru modificarea orelor contactati Inginerul de Testare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 237);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_schimbul3);
+            this.Controls.Add(this.txt_schimbul2);
+            this.Controls.Add(this.txt_schimbul1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Restartare Calculator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_schimbul1;
+        private System.Windows.Forms.TextBox txt_schimbul2;
+        private System.Windows.Forms.TextBox txt_schimbul3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Inchidere Proces/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inchidere_Proces
+{
+    public partial class Form1 : Form
+    {
+        int diferentaOra;
+        int diferentaMinut;
+        int oraSchimbul1= Properties.Settings.Default.oraSchimbul1;
+        int oraSchimbul2= Properties.Settings.Default.oraSchimbul2;
+        int oraSchimbul3= Properties.Settings.Default.oraSchimbul3;
+        int minutSchimbul123 = Properties.Settings.Default.minutSchimbul123;
+        //int minutSchimbul2 = Properties.Settings.Default.minutSchimbul2;
+        //int minutSchimbul3 = Properties.Settings.Default.minutSchimbul3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IncarcareSetari();
+
+        }
+
+        public int Ora()
+        {
+           int ora = DateTime.Now.Hour;
+            return ora;
+        }
+        public int Minut()
+        {
+            int minut = DateTime.Now.Minute;
+            return minut;
+
+        }
+
+        public void IncarcareSetari()
+        {
+            txt_schimbul1.Text = oraSchimbul1.ToString() +" : "+ minutSchimbul123.ToString();
+            txt_schimbul2.Text = oraSchimbul2.ToString() + " : "+ minutSchimbul123.ToString(); ;
+            txt_schimbul3.Text = oraSchimbul3.ToString() + " : "+ minutSchimbul123.ToString(); ;
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((Ora() == oraSchimbul1 || Ora() == oraSchimbul2 || Ora() == oraSchimbul3) && Minut() == minutSchimbul123)
+            {
+                var procesDeInchis = Process.GetProcesses().Where(pro => pro.ProcessName == "notepad"); 
+
+                foreach (var process in procesDeInchis)
+                {
+                    process.Kill();
+                }
+                RestartCalc();
+            }
+        }
+
+        private int DiferentaOra()
+        {
+            if (Ora() < (oraSchimbul1 + 1))
+            {
+                diferentaOra = (oraSchimbul1) - Ora();
+            }
+            else if ((Ora() >= oraSchimbul1 + 1) && (Ora() < oraSchimbul2 + 1))
+            {
+                diferentaOra = oraSchimbul2 - Ora();
+            }
+            else if (Ora() >= oraSchimbul2 + 1 && Ora() < oraSchimbul3 + 1)
+            {
+                diferentaOra = oraSchimbul3 - Ora();
+            }
+            return diferentaOra;
+        }
+        private int DiferentaMinut()
+        {
+            if (Minut() <= 55)
+            {
+                diferentaMinut = 55 - Minut();
+            }
+            else
+            {
+                diferentaMinut =60+55- Minut();
+            }
+           
+            return diferentaMinut;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = DiferentaOra().ToString()+" ore - "+ DiferentaMinut().ToString()+" minute.";
+        }
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            
+            this.WindowState = FormWindowState.Minimized;
+
+            
+            this.Hide();
+        }
+        private void RestartCalc()
+        {
+            System.Diagnostics.Process.Start("shutdown.exe", "-r -t 30 ");
+        }
+    }
+}
